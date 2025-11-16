78d030425cb79b1fa2101a18cf4784d46ac1c72d
Hikmatullo2904 <hikmatulloanvarbekov001@gmail.com>
2025-11-16 21:59:28 +0500

    heartbeat metrics implemented

---
+++ src/main/java/uz/hikmatullo/loadtesting/service/worker/WorkerMetricsService.java
@@ -0,0 +1,22 @@
+package uz.hikmatullo.loadtesting.service.worker;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WorkerMetricsService {
+
+    public double cpuLoad() {
+        // for now, fake data. We will implement it later when we start using metrics
+        return 0.25;
+    }
+
+    public long freeMemory() {
+        // for now, fake data. We will implement it later when we start using metrics
+        return Runtime.getRuntime().freeMemory();
+    }
+
+    public int activeTasks() {
+        // for now, fake data. We will implement it later when we start using metrics
+        return 0;
+    }
+}
